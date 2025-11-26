0eb6b263dc48734e22caa8c08e44f72866259b0d
Dirk Clemens <git@adcore.de>
2025-11-26 19:09:30 +0100

    Initial Commit

---
+++ DrumMonitor/DrumMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrumMonitorApp.swift
+//  DrumMonitor
+//
+//  Created by Dirk Clemens on 26.11.25.
+//
+
+import SwiftUI
+
+@main
+struct DrumMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
